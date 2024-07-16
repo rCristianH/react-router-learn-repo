1160c5417a2afa5de3236a3bc3a7ea4d2f244b08
Cristian Ramirez <ccrhdbj@outlook.com>
2024-07-16 13:17:35 -0500

    auth rutas

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -1,6 +1,12 @@
-
+import { useAuth } from "./auth/auth";
 function AboutPage() {
-  return <h2>About Page</h2>;
+  const auth = useAuth();
+  return (
+    <>
+      <h2>About Page</h2>
+      <p>Welcome back, {auth.user.username}</p>
+    </>
+  );
 }
 
 export { AboutPage };
